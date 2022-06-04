5644a1adf805ec9c40593546d5364ed5f24a9efd
Daniel M Satiro <mateus_satiro@yahoo.com.br>
2022-06-03 23:30:13 -0300

    feat[doc]: begnning part of documentation

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,19 @@ import "express-async-errors";
 import express, { NextFunction, Request, Response } from "express";
 import { errorHandler } from "./errors/errors";
 import registerRouters from "./routes";
+import swaggerUiExpress from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
 registerRouters(app);
+
+app.use(
+  "/api-documentation",
+  swaggerUiExpress.serve,
+  swaggerUiExpress.setup(swaggerDocument)
+);
 
 app.use((err: Error, _: Request, res: Response, __: NextFunction) => {
   return errorHandler(err, res);
